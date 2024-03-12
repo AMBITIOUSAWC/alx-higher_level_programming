a613b128e08d2b15528e3a40cdc47bdacd0fe000
AMBITIOUSAWC <abagnawclement@gmail.com>
2024-03-11 19:52:59 -0700

    7-edges.py

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -2,7 +2,6 @@
 word = "Holberton"
 word_first_3 = word[:3]
 word_last_2 = word[-2:]
-:x
 middle_word = word[1:-1]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
