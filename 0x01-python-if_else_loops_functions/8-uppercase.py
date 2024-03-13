2e487ebdcb344017cd0b34d546493712d06f7378
AMBITIOUSAWC <abagnawclement@gmail.com>
2024-03-12 18:10:54 -0700

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(s):
+    result = ''
+    for char in s:
+        if 'a' <= char <= 'z':
+            char = chr(ord(char) - 32)
+        result += char
+    return result
