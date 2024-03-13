52b99ed1a49b6b3a439dba943aae7cbadff2a3ff
AMBITIOUSAWC <abagnawclement@gmail.com>
2024-03-12 17:13:46 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+number_pairs = []
 for x in range(0, 10):
     for y in range(x + 1, 10):
-        print('{}{}, '.format(x, y), end='')
+        number_pairs.append('{}{}'.format(x, y))
+print(', '.join(number_pairs))
 print('89')
