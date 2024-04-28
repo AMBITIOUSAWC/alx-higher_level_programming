22b3572ce07951e3c92f32326e8cd4d6b0f4de63
AMBITIOUSAWC <abagnawclement@gmail.com>
2024-04-28 10:48:38 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for char in 'abcdefghijklmnopqrstuvwxyz':
-    if char not in ('e', 'q'):
-        print(char, end='')
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) != 'e' and chr(i) != 'q':
+        print(chr(i), end='')
