7f8b8e4ddfd553287c9a2c94ca50a41d0627931b
AMBITIOUSAWC <abagnawclement@gmail.com>
2024-04-28 10:44:10 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if (i == 101) or (i == 113):
-        continue
-    print(chr(i).format(), end="")
+for char in 'abcdefghijklmnopqrstuvwxyz':
+    if char not in ('e', 'q'):
+        print(char, end='')
