17e330155b854fe2c12197e0dea788afb2e6c749
AMBITIOUSAWC <abagnawclement@gmail.com>
2024-05-05 19:31:52 -0700

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,12 +6,14 @@ if __name__ == "__main__":
     b = 5
 
     result_add = add(a, b)
-    result_sub = sub(a, b)
-    result_mul = mul(a, b)
-    result_div = div(a, b)
+    print("{} + {} = {}".format(a, b, result_add))
 
-    print(result_add)
-    print(result_sub)
-    print(result_mul)
-    print(result_div)
+    result_sub = sub(a, b)
+    print("{} - {} = {}".format(a, b, result_sub))
+
+    result_mul = mul(a, b)
+    print("{} * {} = {}".format(a, b, result_mul))
+
+    result_div = div(a, b)
+    print("{} / {} = {}".format(a, b, result_div))
 
