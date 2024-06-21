6a8365228600d0b33e64ff821f9a1e465ae06c76
AMBITIOUSAWC <abagnawclement@gmail.com>
2024-06-21 08:28:22 -0700

    2-remove_database.sql

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,18 +1,2 @@
-#!/bin/bash
-
-DB_NAME="hbtn_0c_0"
-
-# MySQL command to drop database if exists
-MYSQL_CMD="mysql -u root -e 'DROP DATABASE IF EXISTS $DB_NAME;'"
-
-# Execute MySQL command and capture output
-OUTPUT=$(eval $MYSQL_CMD 2>&1)
-
-# Check if there was an error
-if [ $? -ne 0 ]; then
-    echo "Error deleting database $DB_NAME:"
-    echo "$OUTPUT"
-    exit 1
-else
-    echo "Database $DB_NAME deleted successfully (if it existed)."
-fi
+-- Deletes the databases hbtn_0c_0
+BASE IF EXISTS 'hbtn_0c_0';
