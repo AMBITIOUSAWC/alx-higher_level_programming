248255039fac08892971ffafae9aa61634bf3dba
AMBITIOUSAWC <abagnawclement@gmail.com>
2024-07-04 10:34:11 -0700

    ./100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Write a Bash script that sends a request to a URL passed as an argument,
+# and displays only the status code of the response.
 
 # Check if argument is provided
 if [ $# -ne 1 ]; then
