7516f53250935c2219be58ecb9f5f11fe4d736ff
AMBITIOUSAWC <abagnawclement@gmail.com>
2024-07-04 16:26:23 -0700

    Add find_peak function and its test script

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,18 +1,15 @@
 def find_peak(list_of_integers):
-if not list_of_integers:
-return None
+    """Finds a peak in a list of unsorted integers."""
+    if not list_of_integers:
+        return None
 
-def find_peak_util(arr, low, high):
-mid = low + (high - low) // 2
+    def find_peak_util(nums, low, high):
+        mid = (low + high) // 2
+        if (mid == 0 or nums[mid - 1] <= nums[mid]) and (mid == len(nums) - 1 or nums[mid + 1] <= nums[mid]):
+            return nums[mid]
+        elif mid > 0 and nums[mid - 1] > nums[mid]:
+            return find_peak_util(nums, low, mid - 1)
+        else:
+            return find_peak_util(nums, mid + 1, high)
 
-# Check if mid is a peak
-if (mid == 0 or arr[mid - 1] <= arr[mid]) and (mid == len(arr) - 1 or arr[mid + 1] <= arr[mid]):
-return arr[mid]
-# If mid is less than its left neighbor, then the left half must have a peak
-elif mid > 0 and arr[mid - 1] > arr[mid]:
-return find_peak_util(arr, low, mid - 1)
-# If mid is less than its right neighbor, then the right half must have a peak
-else:
-return find_peak_util(arr, mid + 1, high)
-
-return find_peak_util(list_of_integers, 0, len(list_of_integers) - 1)
+    return find_peak_util(list_of_integers, 0, len(list_of_integers) - 1)
