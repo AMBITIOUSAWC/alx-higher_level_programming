debe2d6f51d60e4f7f1beabd8d559c34eacf913b
AMBITIOUSAWC <abagnawclement@gmail.com>
2024-07-04 19:53:50 -0700

    time complexity for my algorithm

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,19 +1,2 @@
 # Write a function that finds a peak in a list of unsorted integers.
 def find_peak(lst):
-    size = len(lst)
-    if size == 0:
-        return None
-    elif size == 1:
-        return lst[0]
-    elif size == 2:
-        return max(lst)
-
-    mid = size // 2
-    peak = lst[mid]
-
-    if peak < lst[mid - 1]:
-        return find_peak(lst[:mid])
-    elif peak < lst[mid + 1]:
-        return find_peak(lst[mid + 1:])
-    else:
-        return peak
