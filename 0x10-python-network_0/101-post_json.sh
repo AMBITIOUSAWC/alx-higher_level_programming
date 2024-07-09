33fbae87b1b93c08ef9a822613aca2b77f30d3ba
AMBITIOUSAWC <abagnawclement@gmail.com>
2024-07-09 06:19:49 -0700

    101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# Write a Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
+# This script sends a JSON POST request to a URL passed as the first argument and displays the body of the response.
+# The JSON data is read from a file passed as the second argument.
+curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
 
-curl -s -H "Content-Type: application/json" -d "$(cat $2)" "$1"
